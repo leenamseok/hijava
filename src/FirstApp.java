c3e4e40e8f0443355ecc7b5a5e0e98c8e7a89928
user <user@LAPTOP-F3US3O49>
2020-06-18 14:52:42 +0900

    4번째

--- src/FirstApp.java
+++ src/FirstApp.java
@@ -11,6 +11,7 @@ public class FirstApp extends A {
 		System.out.println("Main!!");
 		System.out.println("Git Test");
 		System.out.println("Git Author / committer Test user <user@LAPTOP-F3US3O49>");
+		System.out.println("Git 한글 테스트입니다-깃허브에 push합니다.");
 	}
 
 	public void finalize() {
