419003a895d6fc3440bbef03b4455227f2b1874f
leenamseok <digestns@naver.com>
2020-06-18 14:50:03 +0900

    3번째

--- src/FirstApp.java
+++ src/FirstApp.java
@@ -10,6 +10,7 @@ public class FirstApp extends A {
 	public static void main(String[] ar) {
 		System.out.println("Main!!");
 		System.out.println("Git Test");
+		System.out.println("Git Author / committer Test user <user@LAPTOP-F3US3O49>");
 	}
 
 	public void finalize() {
