9d50c8eea3d1237a02c82dc32ce716072072d901
user <user@LAPTOP-F3US3O49>
2020-06-18 14:42:40 +0900

    2nd

--- src/FirstApp.java
+++ src/FirstApp.java
@@ -2,15 +2,16 @@
 class A {
 	static {
 		System.out.println("Static!!");
-	}	
+	}
 }
 
 public class FirstApp extends A {
-	
+
 	public static void main(String[] ar) {
 		System.out.println("Main!!");
+		System.out.println("Git Test");
 	}
-	
+
 	public void finalize() {
 		System.out.println("End");
 	}
